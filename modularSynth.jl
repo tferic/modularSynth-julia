c3e2a7d604d9b6cc091ac90a48fa5dc90dc38c1f
tfc <toni.feric@belsoft.ch>
2025-04-18 23:24:01 +0200

    added generator for dynamic harmonic waves function

--- modularSynth.jl
+++ modularSynth.jl
@@ -16,11 +16,40 @@ struct Note
   generator::Function
 end
 
+struct HarmonicSettings
+  num_harmonics::Int
+  harmonic_amp::Float64
+  falloff::Float64
+end
+
 # === Waveform generators ===
 sine_wave(freq, t) = sin.(2 * pi * freq .* t)
+
 square_wave(freq, t) = sign.(sin.(2 * pi * freq .* t))
-function harmonic_wave(freq, t)
+
+function harmonic_wave_static(freq, t)
   return sin.(2 * pi * freq .* t) + (1/3) * sin.(2 * pi * 3 * freq .* t) + (1/5) * sin.(2 * pi * 5 * freq .* t) + (1/7) * sin.(2 * pi * 7 * freq .* t)
+end
+
+function harmonic_generator(settings::HarmonicSettings)
+  """
+  make_harmonic_generator(num_harmonics::Int, harmonic_amp::Float64, falloff::Float64)
+  function factory: function that generates function dynamically
+
+  Returns a waveform generator function with:
+  - `num_harmonics`: number of odd harmonics (excluding the fundamental)
+  - `harmonic_amp`: amplitude of first harmonic relative to the base frequency
+  - `falloff`: per-harmonic attenuation factor
+  """
+  return function(freq::Float64, t)
+    waveform = sin.(2π * freq .* t)
+    for i in 1:settings.num_harmonics
+      overtone = 2i + 1
+      attenuation = settings.harmonic_amp / (settings.falloff ^ (i - 1))
+      waveform .+= attenuation .* sin.(2π * overtone * freq .* t)
+    end
+    return waveform
+  end
 end
 
 # === Load Configuration ===
@@ -62,7 +91,9 @@ end
 
 function main()
   cfg = load_config("audio_config.toml")
-  note = Note(440.0, 2.0, 0.5, harmonic_wave)
+  harmonic_generator_settings = HarmonicSettings(10, 0.04, 1.4)
+  gen_harmonics = harmonic_generator(harmonic_generator_settings)
+  note = Note(440.0, 2.0, 0.5, gen_harmonics)
   play_note(cfg, note)
 end
 
